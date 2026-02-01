e264c6ffacd1100be538db7a7f8d9d8b0ce90113
Mohamed Halawa <mohamedmahmoudhalawa22@gmail.com>
2026-02-01 11:01:38 +0200

    enhance loader page

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,18 +1,17 @@
-import Image from 'next/image';
+'use client';
+import { useTranslations } from 'next-intl';
+
+import Spinner from '@/components/shared/Spinner';
 
 function Loading() {
+  const t = useTranslations('common.loading');
+
   return (
-    <div className='absolute top-0 z-50 block h-screen w-full overflow-clip bg-primary-500 text-center'>
-      <div className='absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center justify-center gap-3'>
-        <Image
-          src='/gifs/loading.gif'
-          alt='Loading...'
-          width={400}
-          height={400}
-          quality={100}
-          className='object-contain max-xs:size-60'
-        />
-      </div>
+    <div className='flex h-dvh w-dvw flex-col items-center justify-center'>
+      <Spinner />
+      <p className='font-semibold text-grayish-700 md:text-lg xl:text-xl'>
+        {t('text')}
+      </p>
     </div>
   );
 }
